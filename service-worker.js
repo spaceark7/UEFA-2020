b2c5074b5d68e657bb35da64871dec20c1342014
SpaceArk7 <spacearkchief@gmail.com>
2020-11-04 16:09:09 +0700

    responsive view not implemented yet

--- service-worker.js
+++ service-worker.js
@@ -57,3 +57,24 @@ self.addEventListener("activate", function (event) {
     })
   );
 });
+
+self.addEventListener('push', function(event) {
+  var body;
+  if (event.data) {
+    body = event.data.text();
+  } else {
+    body = 'Push message no payload';
+  }
+  var options = {
+    body: body,
+    icon: 'assets/logo.png',
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: 1
+    }
+  };
+  event.waitUntil(
+    self.registration.showNotification('Push Notification', options)
+  );
+});
